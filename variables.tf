46a0149a908b494b60fe4065d60c5e1cfa2282f2
Guilherme Munhoz <guilherme.munhoz@ifood.com.br>
2025-10-07 21:35:48 -0300

    working pipe 4

--- variables.tf
+++ variables.tf
@@ -22,14 +22,13 @@ variable "availability_zones" {
   default     = ["us-east-1a", "us-east-1b"]
 }
 
+variable "terraform_state_bucket" {
+  description = "S3 bucket for Terraform state"
+  type        = string
+}
+
 variable "db_password" {
   description = "Password for the RDS PostgreSQL instance"
   type        = string
   sensitive   = true
-}
-
-variable "lambda_s3_key" {
-  description = "S3 key for Lambda function deployment package"
-  type        = string
-  default     = ""
 }
